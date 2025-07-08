cb2fe7863dfe92c43f246960accc86dad8e5fd09
Asritp5 <154317067+Asritp5@users.noreply.github.com>
2025-07-08 20:01:10 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,14 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import handler404
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('features/', views.features_view, name='features'),
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),
-    path('contact/', views.contact_view, name='contact'),
-    path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile_view, name='profile'),
-    path('about/', views.about_view, name='about'),
-    path('terms/', views.terms_view, name='terms'),
-    path('password-reset/', views.reset_password_view, name='reset_password'),
-    path("dashboard/<str:rqd_user_id>/", views.dashboard_view, name="dashboard"),
-    path('update-visibility/', views.update_visibility, name='update_visibility'),
-    path('update-username/',views.update_username,name="update-username"),
-    path('verify/<str:platform_name>/', views.verify_platform, name='verify_platform'),
-    path('search/', views.search, name='search'),
-    path('delete-platform/<str:platform_name>/', views.delete_platform, name='delete_platform'),
-    path('delete-user/<str:id>',views.delete_user,name='delete-user'),
-
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),  # Include your app's URLs
 ]
+
+if settings.DEBUG:
+    handler404 = 'core.views.error_view'
+    
